bb6a177855c0f416947c240005336e50b20a038b
mironov-nikita <hephaestus31415@gmail.com>
2026-04-13 22:16:44 +0300

    The correct file.
    The correct file. The first time, I loaded the wrong file, which played poorly because it didn't have the greedy strategy epsilon (there was no randomness).

--- main.py
+++ main.py
@@ -27,13 +27,8 @@ if os.path.exists(path):
 else:
     print("No weights found.")
 
-
-
 loss_fn = nn.L1Loss()
-
-optimizer_fn = torch.optim.SGD(
-    params=model.parameters(),
-    lr=0.01) 
+optimizer_fn = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 def WhoHasWon(s):   
@@ -88,48 +83,49 @@ def WhoHasWon(s):
     return False
 
 
-def TicTacToeGame(model):
+def TicTacToeGame(model, epsilon=0.2):
     r = None
-    s = torch.tensor([0,0,0,0,0,0,0,0,0], dtype=torch.float32)
+    s = torch.zeros(9, dtype=torch.float32) 
     MovesHistory = []
+    
     for i in range(9):
-        logits = model(s.unsqueeze(0))
-        logits = logits[0]
-        mask = (s == 0)                      
-        logits_masked = logits.clone()
-        logits_masked[~mask] = -float('inf')  
-        move_index = torch.argmax(logits_masked)
-        move_value = logits_masked[move_index]
+        current_state = s.clone().unsqueeze(0)
+        logits = model(current_state)[0]  
+        mask = (s == 0)
+        free_indices = mask.nonzero(as_tuple=True)[0]
+        if len(free_indices) == 0:
+            break
+
+        if random.random() < epsilon:
+            move_index = free_indices[random.randint(0, len(free_indices) - 1)]
+            move_value = logits[move_index]
+        else:
+            logits_masked = logits.clone()
+            logits_masked[~mask] = -float('inf')
+            move_index = torch.argmax(logits_masked)
+            move_value = logits_masked[move_index]
+        
         MovesHistory.append((move_index, move_value))
 
-        s_new = s.clone()
-        if i & 1:
-            s_new[move_index] = -1
+        if i % 2 == 0:
+            s[move_index] = 1  # Tic
         else:
-            s_new[move_index] = 1
-        s = s_new  
+            s[move_index] = -1 # Toe
 
         Winner = WhoHasWon(s)
-        if not Winner:
-            continue
-        if Winner == "Tic":
-            r = 1
+        if Winner:
+            r = 1 if Winner == "Tic" else -1
             break
-        else:
-            r = -1
-            break
+            
     return r, MovesHistory
 
-
-def MyLoss(MovesHistoryTic, MovesHistoryToe, r, gamma=0.9):
+def MyLoss(MovesHistoryTic, MovesHistoryToe, r, gamma=0.99):
     loss = torch.tensor(0.0, requires_grad=True)
-    
     target_tic = r if r is not None else 0
     for i in reversed(range(len(MovesHistoryTic))):
         move_index, move_value = MovesHistoryTic[i]
         
         loss = loss + (move_value - target_tic)**2
-        
         target_tic *= gamma
 
     target_toe = -r if r is not None else 0
@@ -141,16 +137,17 @@ def MyLoss(MovesHistoryTic, MovesHistoryToe, r, gamma=0.9):
     return loss / 9
 
 
-def train(model, epochs=10):
+
+def train(model, epochs=100):
     model.train()
     print("========== TRAINING STARTS ==========")
     for epoch in range(epochs):
         total_loss = 0.0
-        for i in range(10):
+        for i in range(100):
             optimizer_fn.zero_grad()
 
             r, MovesHistory = TicTacToeGame(model)
-            MovesHistoryTic = MovesHistory[0::2] # even 
+            MovesHistoryTic = MovesHistory[0::2] # even
             MovesHistoryToe = MovesHistory[1::2] # odd
 
             Loss = MyLoss(MovesHistoryTic, MovesHistoryToe, r, 0.9)
@@ -166,98 +163,42 @@ def train(model, epochs=10):
     
 
 def TestTicTacToeModel(model):
-    board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-    ]
-    s = torch.tensor([0,0,0,0,0,0,0,0,0], dtype=torch.float32)  
+    s = torch.zeros(9, dtype=torch.float32)
     model.eval()
+    
     with torch.inference_mode():
-        n = 9
-        FirstMove = random.randint(1, 2)
-        if FirstMove == 1:
-            logits = model(s.unsqueeze(0))  
-            logits = logits[0]  
-            mask = (s == 0)
-            logits[~mask] = -float('inf')
-            move_index = torch.argmax(logits).item()
+        for turn in range(9):
+            print(s.view(3, 3))
             
-            s[move_index] = 1
-            j = move_index // 3
-            i = move_index % 3
-            board[j][i] = 1
-            n -= 1
-        PlayerFlag = 0
-        if n == 8:
-            print("You play with minus ones")
-            PlayerFlag = -1
-        else:
-            print("You play with ones")
-            PlayerFlag = 1
-        for _ in range(n//2):
-            for row in board:
-                print(row)
-            print("Your turn. To make a move, write a number from 1 to 9.")
-            PlayerMove = input("").strip()
-            if PlayerMove not in ('1', '2', '3', '4', '5', '6', '7', '8', '9'):
-                print('Enter correctly, otherwise the game will end.')
-                PlayerMove = input("").strip()
-                if PlayerMove not in ('1', '2', '3', '4', '5', '6', '7', '8', '9'):
-                    print("You entered the coordinate incorrectly the second time.")
-                    return 
+            if turn % 2 == 0:
+                logits = model(s.unsqueeze(0))[0]
+                mask = (s == 0)
+                logits[~mask] = -float('inf')
+                move = torch.argmax(logits).item()
+                s[move] = 1
+                print(f"Model move on {move + 1}")
+            else:
+                move = int(input("Your turn (1-9): ")) - 1
+                s[move] = -1
+            
+            winner = WhoHasWon(s)
+            if winner:
+                print(s.view(3, 3))
+                print(f"{winner} won!")
+                return
                 
-            PlayerMove = int(PlayerMove) - 1  
-            j = PlayerMove // 3
-            i = PlayerMove % 3
-            board[j][i] = PlayerFlag
-            s[PlayerMove] = PlayerFlag
-            Winner = WhoHasWon(s)
-            if Winner == "Tic":
-                for row in board:
-                    print(row)
-                print("The Tic won!")
-                return
-            elif Winner == "Toe":
-                for row in board:
-                    print(row)
-                print("The Toe won!")
-                return
-
-            logits = model(s.unsqueeze(0))[0]
-            mask = (s == 0)
-            logits_masked = logits.clone()
-            logits_masked[~mask] = -float('inf')
-            move_index = torch.argmax(logits_masked).item()
-            
-            j = move_index // 3
-            i = move_index % 3
-            board[j][i] = PlayerFlag * -1
-            s[move_index] = PlayerFlag * -1
-            
-            Winner = WhoHasWon(s)
-            if Winner == "Tic":
-                for row in board:
-                    print(row)
-                print("The Tic won!")
-                return
-            elif Winner == "Toe":
-                for row in board:
-                    print(row)
-                print("The Toe won!")
-                return
-            
-        for row in board:
-            print(row)
-        print("1/2 | 1/2")
-        print("= Draw! =")
+        print("draw!")
 
 if __name__ == '__main__':
-    x = input("Start train: 1\nStart Test: 2\n").strip()
-    if x == "1":
-        train(model)
-    elif x == '2':
-        TestTicTacToeModel(model)
+    while True:
+        print("Start train: 1\nStart Test: 2")
+        x = input().strip()
+        if x == "1":
+            train(model)
+        elif x == '2':
+            TestTicTacToeModel(model)
+
+
 
 
 
